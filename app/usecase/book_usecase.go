57182905da561db0ca156d8f286775ca58b88ce8
Matheus Henrique Raymundo <matheus.raymundo2@br.experian.com>
2021-01-07 10:17:28 -0300

    Book UseCase impl

--- app/usecase/book_usecase.go
+++ app/usecase/book_usecase.go
@@ -1,6 +1,7 @@
 package usecase
 
 import (
+	"context"
 	"time"
 
 	"github.com/matheushr97/golang-clean-architecture/domain"
@@ -9,4 +10,32 @@ import (
 type bookUsecase struct {
 	bookRepo       domain.BookRepository
 	contextTimeout time.Duration
+}
+
+// NewBookUseCase impl of domain.BookUseCase
+func NewBookUseCase(bookRepository domain.BookRepository, timeout time.Duration) domain.BookUseCase {
+	return &bookUsecase{
+		bookRepo:       bookRepository,
+		contextTimeout: timeout,
+	}
+}
+
+func (uc *bookUsecase) Create(book domain.Book) (res *domain.Book, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), uc.contextTimeout)
+	defer cancel()
+	// ALL BUSINESS RULES HERE
+
+	return uc.bookRepo.Create(ctx, book)
+}
+
+func (uc *bookUsecase) Fetch() (res *[]domain.Book, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), uc.contextTimeout)
+	defer cancel()
+	return uc.bookRepo.Fetch(ctx)
+}
+
+func (uc *bookUsecase) GetByID(id string) (*domain.Book, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), uc.contextTimeout)
+	defer cancel()
+	return uc.bookRepo.GetByID(ctx, id)
 }
--- domain/book.go
+++ domain/book.go
@@ -17,9 +17,9 @@ type Book struct {
 
 // BookUseCase repository to save book entity
 type BookUseCase interface {
-	Create(ctx context.Context) (res Book, err error)
-	Fetch(ctx context.Context) (res []Book, err error)
-	GetByID(ctx context.Context, id string) (Book, error)
+	Create(Book) (res *Book, err error)
+	Fetch() (res *[]Book, err error)
+	GetByID(id string) (*Book, error)
 }
 
 // BookRepository repository to save book entity
